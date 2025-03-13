7be754275d51f04a3a09f077310d685f6aa3b027
정현우 <slander.jung@gmail.com>
2025-03-13 19:40:32 +0900

    [feat] 기본 움직임 구현
    이동, 점멸 등 구현

--- Assets/Scripts/JHW/PlayerCommand.cs
+++ Assets/Scripts/JHW/PlayerCommand.cs
@@ -2,239 +2,144 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum State
+public class CommandBase
 {
-    Neutral, //기본
-    Slow, //둔화
-    Root, //속박
-    Stun, //기절
-    Airborne, //에어본
-    Invincible//무적
+    protected PlayerController controller;
+    protected Character character;
+    public virtual void Execute()
+    {
+
+    }
+
+    public CommandBase(PlayerController controller)
+    {
+        this.controller = controller;
+        character = controller.character;
+    }
 }
-public class Character
+
+public class MoveCommand : CommandBase //움직임
 {
-    float _HP; //체력
-    float _curHP; //현재 체력
-    float _HPRegen; //체젠
-    float _atk; //공격력
-    int _lethality; //물관
-    int _armorPen; //방깎 (35%면 0.35f 이렇게 되게 할거)
-    float _def; //방어력
-    int _moveSpeed; //이속
-    float _atkSpeed; //공속 (얘도 0.00f)
-    float _baseAS; //기본공속 (얘도 0.00f)
-    float _additionAS; //추가공속 (얘도 0.00f)
-    float _critChance; //치확 (얘도 0.00f)
-    float _critDamage; //치뎀 (얘는 1.30f 이렇게 할듯)
-    int _range; //사거리
-    int _abilityHaste; //스킬가속
-    float _lifeSteal; //피흡 (얘도 0.00f)
-    State _state; //상태(이상)
-
-    float qCoolDown;
-    float qCurCool;
-    float wCoolDown;
-    float wCurCool;
-    float eCoolDown;
-    float eCurCool;
-    float rCoolDown;
-    float rCurCool;
-
-    bool canRush;
-    bool canFlash;
-
-    public float HP
+    Vector3 movePos;
+    public MoveCommand(PlayerController controller, Vector3 movePos) : base(controller)
     {
-        get
-        {
-            return _HP;
-        }
+        this.movePos = movePos;
     }
 
-    public float CurHP
+    public override void Execute()
     {
-        get
-        {
-            return _curHP;
-        }
+        controller.Move(movePos);
     }
 
-    public float ATK
+}
+
+public class AutoAttackCommand : CommandBase //평타
+{
+    Character target;
+    public AutoAttackCommand(PlayerController controller, Character target) : base(controller)
     {
-        get
-        {
-            return _atk;
-        }
+        this.target = target;
     }
 
-    public float DEF
+    public override void Execute()
     {
-        get
-        {
-            return _def;
-        }
+        controller.AutoAttack(target);
+    }
+}
+
+public class RushCommand : CommandBase //도주(유체화)
+{
+    public RushCommand(PlayerController controller) : base(controller)
+    {
+
     }
 
-    public float LifeSteal
+    public override void Execute()
     {
-        get
-        {
-            return _lifeSteal;
-        }
+        controller.Rush();
+    }
+}
+
+
+public class FlashCommmand : CommandBase //점멸
+{
+    Vector3 pos;
+
+    public FlashCommmand(PlayerController controller, Vector3 pos) : base(controller)
+    {
+        this.pos = pos;
     }
 
-    public Character(float HP, float hPRegen, float atk, float def, int moveSpeed, float baseAS, int range, float qCoolDown, float wCoolDown, float eCoolDown, float rCoolDown)
+    public override void Execute()
     {
-        _HP = HP;
-        _curHP = _HP;
-        _HPRegen = hPRegen;
-        _atk = atk;
-        _lethality = 0;
-        _armorPen = 0;
-        _def = def;
-        _moveSpeed = moveSpeed;
-        _atkSpeed = baseAS;
-        _baseAS = baseAS;
-        _additionAS = 0;
-        _critChance = 0;
-        _critDamage = 1.7f;
-        _range = range;
-        _abilityHaste = 0;
-        _lifeSteal = 0;
-        _state = State.Neutral;
+        controller.Flash(pos);
+    }
+}
 
-        this.qCoolDown = qCoolDown;
-        this.wCoolDown = wCoolDown;
-        this.eCoolDown = eCoolDown;
-        this.rCoolDown = rCoolDown;
+public class SKillCommands : CommandBase //스킬들 베이스
+{
+    protected bool isTargeting;
+    protected bool isChanneling;
+    protected Character target;
+    protected Vector3 location;
 
-        eCurCool = 0;
-        wCurCool = 0;
-        qCurCool = 0;
-        rCurCool = 0;
+    public SKillCommands(PlayerController controller, bool isTargeting, bool isChanneling, Character target, Vector3 location) : base(controller)
+    {
+        this.isTargeting = isTargeting;
+        this.isChanneling = isChanneling;
+        this.target = target;
+        this.location = location;
+    }
+}
+
+public class SkillQCommand : SKillCommands //Q스킬
+{
+    public SkillQCommand(PlayerController controller, bool isTargeting, bool isChanneling, Character target, Vector3 location) : base(controller, isTargeting, isChanneling, target, location)
+    {
+
     }
 
-    public void Move(Transform transform)
+    public override void Execute()
     {
-        //움직이다
+        controller.SkillQ(isTargeting, isChanneling, target, location);
+    }
+}
+
+public class SkillWCommand : SKillCommands //W스킬
+{
+    public SkillWCommand(PlayerController controller, bool isTargeting, bool isChanneling, Character target, Vector3 location) : base(controller, isTargeting, isChanneling, target, location)
+    {
+
     }
 
-    public virtual void AutoAttack(Character target)
+    public override void Execute()
     {
-        //평타
-        float damage = _atk;
+        controller.SkillW(isTargeting, isChanneling, target, location);
+    }
+}
 
-        if (_critChance >= Random.Range(0f, 1f)) //치명타
-        {
-            damage *= _critDamage;
-        }
+public class SkillECommand : SKillCommands //E스킬
+{
+    public SkillECommand(PlayerController controller, bool isTargeting, bool isChanneling, Character target, Vector3 location) : base(controller, isTargeting, isChanneling, target, location)
+    {
 
-        target.TakeDamage(damage, false, _lethality, _armorPen);
     }
 
-    public virtual void SkillQ(bool isTargeting, bool isChanneling, Character target, Vector3 location)
+    public override void Execute()
     {
-        if (qCurCool >= 0)
-        {
-            //대충 스킬쓰기
-            qCurCool = 100 / (100 + _abilityHaste) * qCoolDown;
-        }
-        else
-        {
-            //못쓴다하기
-        }
+        controller.SkillE(isTargeting, isChanneling, target, location);
+    }
+}
+
+public class SkillRCommand : SKillCommands //R스킬
+{
+    public SkillRCommand(PlayerController controller, bool isTargeting, bool isChanneling, Character target, Vector3 location) : base(controller, isTargeting, isChanneling, target, location)
+    {
+
     }
 
-    public virtual void SkillW(bool isTargeting, bool isChanneling, Character target, Vector3 location)
+    public override void Execute()
     {
-        if (wCurCool >= 0)
-        {
-            //대충 스킬쓰기
-            wCurCool = 100 / (100 + _abilityHaste) * wCoolDown;
-        }
-        else
-        {
-            //못쓴다하기
-        }
-    }
-
-    public virtual void SkillE(bool isTargeting, bool isChanneling, Character target, Vector3 location)
-    {
-        if (eCurCool >= 0)
-        {
-            //대충 스킬쓰기
-            eCurCool = 100 / (100 + _abilityHaste) * eCoolDown;
-        }
-        else
-        {
-            //못쓴다하기
-        }
-    }
-
-    public virtual void SkillR(bool isTargeting, bool isChanneling, Character target, Vector3 location)
-    {
-        if (rCurCool >= 0)
-        {
-            //대충 스킬쓰기
-            rCurCool = 100 / (100 + _abilityHaste) * rCoolDown;
-        }
-        else
-        {
-            //못쓴다하기
-        }
-    }
-
-    public void Rush()
-    {
-        //도주
-        
-    }
-
-    IEnumerator DoRush()
-    {
-        int tempSpeed = (int)(_moveSpeed * 0.4f);
-        _moveSpeed += tempSpeed;
-        yield return new WaitForSeconds(2f);
-        _moveSpeed -= tempSpeed;
-    }
-
-    public void Flash(Vector3 pos)
-    {
-        //점멸
-    }
-
-    public void TakeDamage(float damage, bool isTrueDmg, int lethal, float armorPen)
-    {
-        if (isTrueDmg)
-        {
-            _curHP -= damage;
-        }
-        else
-        {
-            float tempDef = _def - lethal; //물관 적용
-            tempDef -= tempDef * armorPen; //방관 적용
-            _curHP -= damage * (1 + tempDef * 0.01f);
-
-        }
-
-        if (_curHP <= 0)
-        {
-            _curHP = 0;
-            Die();
-        }
-    }
-
-    public void Heal(float heal)
-    {
-        _curHP += heal;
-        if (_curHP >= _HP)
-        {
-            _curHP = _HP;
-        }
-    }
-
-    void Die()
-    {
-        //죽으셈
+        controller.SkillR(isTargeting, isChanneling, target, location);
     }
 }
--- Assets/Scripts/JHW/PlayerController.cs
+++ Assets/Scripts/JHW/PlayerController.cs
@@ -1,18 +1,226 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
+    public Character character;
+    NavMeshAgent agent;
+
+    CommandBase toExecute;
+
+    MoveCommand moveCommand;
+    AutoAttackCommand autoAttackCommand;
+    SkillQCommand qCommand;
+    SkillWCommand wCommand;
+    SkillECommand eCommand;
+    SkillRCommand rCommand;
+    RushCommand rushCommand;
+    FlashCommmand flashCommmand;
+
+    Ray ray;
+    RaycastHit hit;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        character.InitCharacter();
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = character.MoveSpeed * 0.01f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButton(1))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.gameObject.CompareTag("Enemy"))
+                {
+                    toExecute = new AutoAttackCommand(this, hit.transform.GetComponent<PlayerController>().character);
+                    toExecute.Execute();
+                }
+                else
+                {
+                    toExecute = new MoveCommand(this, hit.point);
+                    toExecute.Execute();
+                }
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                toExecute = new SkillQCommand(this, false, false, hit.transform.GetComponent<PlayerController>()?.character, hit.point);
+                toExecute.Execute();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                toExecute = new SkillWCommand(this, false, false, hit.transform.GetComponent<PlayerController>()?.character, hit.point);
+                toExecute.Execute();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                toExecute = new SkillECommand(this, false, false, hit.transform.GetComponent<PlayerController>()?.character, hit.point);
+                toExecute.Execute();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                toExecute = new SkillRCommand(this, false, false, hit.transform.GetComponent<PlayerController>()?.character, hit.point);
+                toExecute.Execute();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                toExecute = new RushCommand(this);
+                toExecute.Execute();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                toExecute = new FlashCommmand(this, hit.point);
+                toExecute.Execute();
+            }
+        }
     }
+    public virtual void Move(Vector3 pos)
+    {
+        //움직이다 sibal
+        agent.SetDestination(pos);
+    }
+
+    public virtual void AutoAttack(Character target)
+    {
+        Debug.Log("AA " + target.name);
+        //평타
+        float damage = character.ATK;
+
+        if (character.CritChance >= Random.Range(0f, 1f)) //치명타
+        {
+            damage *= character.CritDamage;
+        }
+
+        target.TakeDamage(damage, false, character.Lethality, character.ArmorPenetration);
+    }
+
+    public virtual void SkillQ(bool isTargeting, bool isChanneling, Character target, Vector3 location)
+    {
+        if (character.CurQCool >= 0)
+        {
+            //대충 스킬쓰기
+            character.SetQCooldown();
+        }
+        else
+        {
+            //못쓴다하기
+        }
+    }
+
+    public virtual void SkillW(bool isTargeting, bool isChanneling, Character target, Vector3 location)
+    {
+        if (character.CurWCool >= 0)
+        {
+            //대충 스킬쓰기
+            character.SetWCooldown();
+        }
+        else
+        {
+            //못쓴다하기
+        }
+    }
+
+    public virtual void SkillE(bool isTargeting, bool isChanneling, Character target, Vector3 location)
+    {
+        if (character.CurECool >= 0)
+        {
+            //대충 스킬쓰기
+            character.SetECooldown();
+        }
+        else
+        {
+            //못쓴다하기
+        }
+    }
+
+    public virtual void SkillR(bool isTargeting, bool isChanneling, Character target, Vector3 location)
+    {
+        if (character.CurRCool >= 0)
+        {
+            //대충 스킬쓰기
+            character.SetRCooldown();
+        }
+        else
+        {
+            //못쓴다하기
+        }
+    }
+
+    public void Rush()
+    {
+        //도주
+        if (character.CanRush)
+        {
+            StartCoroutine(DoRush());
+        }
+        else
+        {
+            //못쓴다고 하기
+        }
+    }
+
+    IEnumerator DoRush()
+    {
+        character.SetCanRush(false);
+        int tempSpeed = (int)(character.MoveSpeed * 0.4f);
+        character.AdjustMoveSpeed(tempSpeed);
+        yield return new WaitForSeconds(2f);
+        character.AdjustMoveSpeed(-tempSpeed);
+    }
+
+    public void Flash(Vector3 pos)
+    {
+        //점멸
+        if (character.CanFlash)
+        {
+            agent.ResetPath();
+            transform.position = pos;
+        }
+        else
+        {
+            //못쓴다고 하기
+        }
+    }
+
 }
