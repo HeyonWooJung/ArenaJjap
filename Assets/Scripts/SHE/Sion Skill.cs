9f101234fc83ad5fd906f09c62230ca42629fdd4
Camembertkimchi <83268157+Camembertkimchi@users.noreply.github.com>
2025-03-17 18:01:01 +0900

    [Feat]사붕이 Q 구현
    이제 이미지로 띄워주기만 하면 끝!

--- Assets/Scripts/SHE/Sion Skill.cs
+++ Assets/Scripts/SHE/Sion Skill.cs
@@ -1,3 +1,4 @@
+using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -7,7 +8,9 @@ using UnityEngine.UIElements;
 public class SionSkill : MonoBehaviour
 {
     Animator anim;
-    
+    [SerializeField] LayerMask hitLayer;
+    [SerializeField] bool debuggingMode;
+ 
     [SerializeField] float qSkillMinFixedDamage = 120;
     [SerializeField] float qSkillMaxFixedDamage = 350;
     [SerializeField] float qSkillMinAddDamage = 0.8f;
@@ -20,12 +23,16 @@ public class SionSkill : MonoBehaviour
     [SerializeField] float qSkillCurDamage;
     [SerializeField] float qSkillCurAirborne;
     [SerializeField] float qSkillCurStun;
+    [SerializeField] float qSkillSlowPercentage = 75f;
+    //[SerializeField] bool qSkillAirborne;
     float qSkillTimer;
     float maxChargeTime = 2f;
     bool qSkillCharging = false;
-    [SerializeField] float qSkillMinSize;
-    [SerializeField] float qSkillMaxSize;
-    [SerializeField] Vector3 qSkillOriginalCenterPos;
+    [SerializeField] float qSkillMinLength;
+    [SerializeField] float qSkillMaxLength;
+    [SerializeField] float qSkillWidth;
+    //[SerializeField] Vector3 qSkillOriginalCenterPos;
+   
 
     [SerializeField] float wBarrier;
     [SerializeField] float wTimer;
@@ -34,22 +41,43 @@ public class SionSkill : MonoBehaviour
 
 
 
-    [SerializeField]Character charcter;
-    [SerializeField] BoxCollider qSkillcol;
+    [SerializeField]Character character;
+    //[SerializeField] BoxCollider qSkillcol;
     
     Dictionary<string, Character> characterDictionary = new Dictionary<string, Character>();
     void Start()
     {
         //Red Blue tag 찾고
-        characterDictionary.Add("Blue", charcter);
-        qSkillcol = GetComponent<BoxCollider>();
-        qSkillOriginalCenterPos = qSkillcol.center;
+        //characterDictionary.Add("Blue", charcter);
+        //qSkillcol = GetComponent<BoxCollider>();
+        //qSkillOriginalCenterPos = qSkillcol.center;
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            QSkill();
+        }
+    }
+
+    public void UpdateEnemyDictionary()
+    {
+        //Red Blue tag찾아요
+        if(this.gameObject.tag == "Blue1" || this.gameObject.tag == "Blue2")
+        {
+            characterDictionary.Clear();
+            characterDictionary.Add("Red1", character);
+            characterDictionary.Add("Red2", character);
+        }
+        else
+        {
+            characterDictionary.Clear();
+            characterDictionary.Add("Blue1", character);
+            characterDictionary.Add("Blue2", character);
+        }
     }
 
     
@@ -58,6 +86,7 @@ public class SionSkill : MonoBehaviour
     {
         if(!qSkillCharging)
         {
+            GetMouseCursorPos();
             StartCoroutine(QSkillCharge());
         }
         
@@ -66,43 +95,124 @@ public class SionSkill : MonoBehaviour
     IEnumerator QSkillCharge()
     {
         qSkillTimer = 0;
+        qSkillCharging = true;
+        
         anim.SetTrigger("Q");
-        while(qSkillCharging == true)
+
+        while(Input.GetKey(KeyCode.Q) && qSkillTimer <= 2f)
         {
             qSkillTimer += Time.deltaTime;
-            float chargeRatio = Mathf.Clamp01(qSkillTimer / 1f);
-            qSkillcol.size = new Vector3(qSkillcol.size.x, qSkillcol.size.y, Mathf.Lerp(qSkillMinSize, qSkillMaxSize, chargeRatio));
-            qSkillcol.center = new Vector3(qSkillcol.center.x, qSkillcol.center.y, Mathf.Lerp(qSkillcol.size.z,qSkillcol.size.z + qSkillMaxSize, chargeRatio));
-            qSkillCurDamage = Mathf.Lerp(qSkillMinFixedDamage + qSkillMinAddDamage, qSkillMaxFixedDamage + qSkillMaxAddDamage, qSkillTimer);
-            
             yield return null;
         }
-        
-        if(qSkillCharging == false)//이 부분은 소통상 함수에 들어가야할 수도 있음 ㅇㅇ
+
+        CastQSkill();
+        qSkillCharging = false;
+
+        #region 콜라이더로 하려다가 말았다
+        //qSkillcol.center = qSkillOriginalCenterPos;
+        //while(qSkillCharging == true)
+        //{
+        //    qSkillTimer += Time.deltaTime;
+        //    float chargeRatio = Mathf.Clamp01(qSkillTimer / 1f);
+        //    qSkillcol.size = new Vector3(qSkillcol.size.x, qSkillcol.size.y, Mathf.Lerp(qSkillMinSize, qSkillMaxSize, chargeRatio));
+        //    qSkillcol.center = new Vector3(qSkillcol.center.x, qSkillcol.center.y, Mathf.Lerp(qSkillcol.size.z,qSkillcol.size.z + qSkillMaxSize, chargeRatio));
+        //    qSkillCurDamage = Mathf.Lerp(qSkillMinFixedDamage + qSkillMinAddDamage, qSkillMaxFixedDamage + qSkillMaxAddDamage, qSkillTimer);
+        //    
+        //    yield return null;
+        //}
+        //
+        //if(qSkillCharging == false)//이 부분은 소통상 함수에 들어가야할 수도 있음 ㅇㅇ
+        //{
+        //    if(qSkillTimer >= 1f)
+        //    {
+        //        //대충 에어본 쌉가능
+        //        //기절도 드가시고 ㅋ
+        //    }
+        //    else
+        //    {
+        //        //대충 슬로우 넣는다 ㅇㅇ
+        //    }
+        //}
+        //
+        //qSkillcol.enabled = true;
+        #endregion
+    }
+
+    void CastQSkill()
+    {
+        //비율 0~1
+        float chargeRatio = Mathf.Clamp01(qSkillTimer / 1f);
+        //스킬 넓이
+        float skillLength = Mathf.Lerp(qSkillMinLength, qSkillMaxLength, chargeRatio);
+
+        Vector3 boxCenter = transform.position + transform.forward * (skillLength / 2f);
+        Vector3 halfExtents = new Vector3(qSkillWidth / 2f, 0.5f, skillLength / 2f);
+        Quaternion boxOrientation = transform.rotation;
+
+
+
+        qSkillCurDamage = Mathf.Lerp(qSkillMinFixedDamage, qSkillMaxFixedDamage, qSkillTimer) + Mathf.Lerp(qSkillMinAddDamage, qSkillMaxAddDamage, qSkillChargeTime);
+        if(qSkillTimer >= 1)
         {
-            if(qSkillTimer >= 1f)
+            qSkillCurAirborne = Mathf.Lerp(qSkillAirborneTimeMin, qSkillAirborneTimeMax, chargeRatio / 2f);
+            qSkillCurStun = Mathf.Lerp(qSkillStunTimeMix, qSkillStunTimeMax, qSkillTimer / 2f) + qSkillCurAirborne;
+        }
+
+
+        Collider[] hits = Physics.OverlapBox(boxCenter, halfExtents, boxOrientation, hitLayer);
+
+        
+
+        foreach(Collider hit in hits)
+        {
+            if (characterDictionary.TryGetValue(hit.tag, out Character enemy) == true)
             {
-                //대충 에어본 쌉가능
-                //기절도 드가시고 ㅋ
+                if (qSkillTimer >= 1)
+                {
+                    //에어본 + 기절
+
+
+                    enemy.SetState(State.Airborne);
+                    enemy.SetState(State.Stun);
+                   
+                }
+                
             }
             else
             {
-                //대충 슬로우 넣는다 ㅇㅇ
+                    enemy.SetState(State.Slow);
             }
+            Debug.Log(hit.tag);
+            enemy.TakeDamage(qSkillCurDamage, false, character.Lethality, character.ArmorPenetration);
+        }
+        
+
+
+
+        if (qSkillTimer >= 1)
+        {
+            Debug.Log("Full charging Q!");
+            anim.SetTrigger("QLong");
+        }
+        else
+        {
+            Debug.Log("짧게 쓰기");
+            anim.SetTrigger("QShort");
         }
 
-        qSkillcol.enabled = true;
-        
+
+
     }
 
-    Vector3 GetMouseCursorPos()
+    void GetMouseCursorPos()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if(Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity))
+        if (Physics.Raycast(ray, out RaycastHit hit, 100f))
         {
-            return hit.point;
+            Vector3 lookDir = (hit.point - transform.position).normalized;
+            lookDir.y = 0; // 높이 변화 무시 (수평 방향만 고려)
+            transform.rotation = Quaternion.LookRotation(lookDir);
         }
-        return Vector3.zero;
     }
 
 
@@ -115,5 +225,27 @@ public class SionSkill : MonoBehaviour
 
     }
 
+    void OnDrawGizmos()
+    {
+        if (!debuggingMode) return;
+
+        //비율 0~1
+        float chargeRatio = Mathf.Clamp01(qSkillTimer / 1f);
+        //스킬 넓이
+        float skillLength = Mathf.Lerp(qSkillMinLength, qSkillMaxLength, chargeRatio);
+
+        Vector3 boxCenter = transform.position + transform.forward * (skillLength / 2f);
+        Vector3 halfExtents = new Vector3(qSkillWidth / 2f, 0.5f, skillLength / 2f);
+        Quaternion boxOrientation = transform.rotation;
+
+        Gizmos.color = Color.green;
+            Matrix4x4 oldMatrix = Gizmos.matrix;
+            Gizmos.matrix = Matrix4x4.TRS(boxCenter, boxOrientation, Vector3.one);
+            Gizmos.DrawWireCube(Vector3.zero, halfExtents * 2f);
+            Gizmos.matrix = oldMatrix;
+        
+    }
+
 
 }
+   
