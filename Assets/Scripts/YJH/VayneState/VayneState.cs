8c352c3ece9716b2f291a79938b2762afad63f45
jonghyun109 <mimj2527@gmail.com>
2025-03-24 11:35:33 +0900

    [feat] 상태패턴 추가

--- Assets/Scripts/YJH/VayneState/VayneState.cs
+++ Assets/Scripts/YJH/VayneState/VayneState.cs
@@ -2,17 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VayneState : MonoBehaviour
+public interface IVayneState
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public void EnterState(VayneState VS) { }
+    public void UpdateState() { }
+    public void ExitState() { }
+}
 
-    // Update is called once per frame
-    void Update()
+public class VayneState : PlayerController
+{
+    IVayneState currentState;
+
+    [Header("애니메이션")]
+    public Animator anim;
+
+    private void Start()
     {
-        
+        //처음 상태
+        //ChangeState();
+    }
+    public void ChangeState(IVayneState newState)
+    {
+        if(currentState != null)
+        {
+            currentState.ExitState();
+        }
+        currentState = newState;
+        currentState.EnterState(this);
     }
 }
